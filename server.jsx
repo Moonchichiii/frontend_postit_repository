08db56f106040c6708b34a3c9b91750b56614b88
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-01-27 11:19:26 +0100

    update to server.jsx,

--- server.jsx
+++ server.jsx
@@ -1,12 +1,15 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve the static files from the Vite build
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Serve your index.html file at all other routes (for a SPA)
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
